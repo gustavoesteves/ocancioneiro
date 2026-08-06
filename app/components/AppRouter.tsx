c0f65b991412b694ca39d7fef6de0b9c181d7105
Gustavo Esteves <gustavo.esteves@gmail.com>
2026-08-06 19:21:59 -0300

    Fix local import route

--- app/components/AppRouter.tsx
+++ app/components/AppRouter.tsx
@@ -7,7 +7,7 @@ export function AppRouter() {
   const pathname =
     typeof window === "undefined" ? "/" : window.location.pathname.replace(/\/$/, "");
 
-  if (pathname.endsWith("/import")) {
+  if (pathname.endsWith("/import") || pathname.endsWith("/import/index.html")) {
     return <ImportTool />;
   }
 
---
+++ app/import/page.tsx
@@ -0,0 +1,5 @@
+import { ImportTool } from "../components/ImportTool";
+
+export default function ImportPage() {
+  return <ImportTool />;
+}
